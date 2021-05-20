5ba37762af059b725cec4cd993b324a9a8a869a9
刘智伟 <a974723617@163.com>
2021-05-20 15:21:24 +0800

    配置ribbon负载策略

--- cloud-consumer-order80/src/main/java/com/lzw/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/lzw/springcloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.lzw.springcloud.controller;
 
 import com.lzw.springcloud.entity.CommentResult;
 import com.lzw.springcloud.entity.Payment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +22,16 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommentResult<Payment> selectById(@PathVariable("id") Long id){
         return  restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommentResult.class);
+    }
+    @GetMapping("/consumer/entity/payment/get/{id}")
+    public CommentResult<Payment> selectEntityById(@PathVariable("id")Long id){
+        ResponseEntity<CommentResult> forEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommentResult.class);
+        if(forEntity.getStatusCode().is2xxSuccessful()){
+            return forEntity.getBody();
+        }else{
+            return new CommentResult(444,"操作失败！");
+        }
+
     }
 }
 
