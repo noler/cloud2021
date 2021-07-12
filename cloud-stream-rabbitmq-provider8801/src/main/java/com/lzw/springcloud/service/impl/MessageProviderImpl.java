5dadd1cadff71cd3cc9a93812c5a6f569ecbf03c
刘智伟 <a974723617@163.com>
2021-07-12 15:00:49 +0800

    新增了Spring Stream 消费驱动的生产者与消费者

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/lzw/springcloud/service/impl/MessageProviderImpl.java
@@ -0,0 +1,25 @@
+package com.lzw.springcloud.service.impl;
+
+import com.lzw.springcloud.service.IMessageProvider;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+@Component
+@EnableBinding(Source.class)
+public class MessageProviderImpl implements IMessageProvider {
+    @Resource
+    private MessageChannel output;
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(serial).build());
+        System.out.println("********serial:"+serial);
+        return null;
+    }
+}
